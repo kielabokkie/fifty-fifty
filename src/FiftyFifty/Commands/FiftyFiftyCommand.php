c01fae47b48923948ba751f638996d081d31ca66
Damian Peterson <dp@pixelfusion.co.nz>
2016-04-21 13:41:21 +1200

    change rules so that it draws from a reducing pool of money and stops when you double or run out

--- src/FiftyFifty/Commands/FiftyFiftyCommand.php
+++ src/FiftyFifty/Commands/FiftyFiftyCommand.php
@@ -2,7 +2,6 @@
 
 namespace FiftyFifty\Commands;
 
-use Faker\Factory;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -17,37 +16,76 @@ class FiftyFiftyCommand extends Command
             ->setName('gamble')
             ->setDescription('Gamble')
             ->addArgument(
+                'max_spend',
+                null,
+                'How much are you prepared to spend',
+                1
+            )
+            ->addArgument(
+                'starting_amount',
+                null,
+                'What is the minimum you can gamble?',
+                0.00000001
+            )
+            ->addArgument(
                 'iterations',
                 null,
-                'How many times should the message be printed?',
-                100000
+                'How many times do you want to play?',
+                1
             )
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $iterations = $input->getArgument('iterations');
-
-        $faker = Factory::create();
-        $results = [];
-
-        for ($x=0; $x < $iterations; $x++) {
-            for ($i=0; $i <= 50; $i++) {
-                // True or false
-                $boolean = $faker->boolean(50);
-
-                if (isset($results[$i]) === false) {
-                    $results[$i] = 0;
-                }
-
-                if ($boolean === true) {
-                    $results[$i] = $results[$i]+1;
-                    break;
-                }
+        $maxSpend = (float)$input->getArgument('max_spend');
+        $startingAmount = (float)$input->getArgument('starting_amount');
+        $iterations = (int)$input->getArgument('iterations');
+        
+        $wins = 0;
+        $losses = 0;
+        
+        for ($i = 0; $i < $iterations; $i++) {
+            if ($this->gamble($maxSpend, $startingAmount, ($iterations === 1) ? $output : null)) {
+                $wins++;
+            } else {
+                $losses++;
             }
         }
 
-        $output->write(print_r($results));
+        $output->writeln(sprintf("wins: %d, losses: %d", $wins, $losses));
+    }
+
+    public function gamble($maxSpend, $startingAmount, $output = null)
+    {
+        $remainingAmount = $maxSpend;
+        $currentBet = $startingAmount;
+        $numberOfBets = 0;
+
+        while (true) {
+            if ((mt_rand(0, 1) === 1)) {
+                $remainingAmount -= ($currentBet + ($currentBet * 0.001));
+            } else {
+                $remainingAmount += ($currentBet - ($currentBet * 0.001));
+            }
+            if ($output) {
+                $output->writeln(sprintf("#%d : %s : %s", $numberOfBets, $currentBet, $remainingAmount));
+            }
+            if ($remainingAmount > ($maxSpend * 2)) {
+                if ($output) {
+                    $output->writeln('win');
+                }
+                return true;
+                break;
+            } elseif ($remainingAmount < 0) {
+                if ($output) {
+                    $output->writeln('lose');
+                }
+                return false;
+                break;
+            }
+            $currentBet *= 2;
+            $numberOfBets++;
+        }
     }
 }
